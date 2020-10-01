016e319ba5e8c0a78a2bbf20adfb64d6dafc946d
tsereiva <zvejys7@gmail.com>
2020-09-30 23:32:53 -0400

    Added print statements in order to visualize lambda function

--- DataSciencester.py
+++ DataSciencester.py
@@ -42,8 +42,9 @@ print(avg_connections)
 num_friends_by_id = [(user["id"], number_of_friends(user))
                      for user in users]
 
+print(num_friends_by_id)
+
 num_friends_by_id.sort(                                # Sort the list
        key=lambda id_and_friends: id_and_friends[1],   # by num_friends
        reverse=True)                                   # largest to smallest
-
 print(num_friends_by_id) #Visualized new sorted list
